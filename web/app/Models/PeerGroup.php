893fd0ec71cf8c42e3ea0a042544a504df3461ca
Sunwoo Park <swparkaust@me.com>
2022-03-14 21:17:33 -0700

    Define many-to-many relationships

--- web/app/Models/PeerGroup.php
+++ web/app/Models/PeerGroup.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class PeerGroup extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
