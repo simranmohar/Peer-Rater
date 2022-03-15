2b8c9865666c5ad46e2f1136ba0aa1d65fd96a7d
Sunwoo Park <swparkaust@me.com>
2022-03-15 00:24:06 -0700

    Fix Target class does not exist

--- web/routes/api.php
+++ web/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PeerGroupController;
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,25 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/peer-groups', 'PeerGroupController@all')->name('peer_groups.all');
+Route::get('/peer-groups', [PeerGroupController::class, 'all'])->name('peer_groups.all');
 
-Route::post('/peer-groups', 'PeerGroupController@store')->name('peer_groups.store');
+Route::post('/peer-groups', [PeerGroupController::class, 'store'])->name('peer_groups.store');
 
-Route::get('/peer-groups/{peerGroup}', 'PeerGroupController@show')->name('peer_groups.show');
+Route::get('/peer-groups/{peerGroup}', [PeerGroupController::class, 'show'])->name('peer_groups.show');
 
-Route::put('/peer-groups/{peerGroup}', 'PeerGroupController@update')->name('peer_groups.update');
+Route::put('/peer-groups/{peerGroup}', [PeerGroupController::class, 'update'])->name('peer_groups.update');
 
-Route::delete('/peer-groups/{peerGroup}', 'PeerGroupController@destory')->name('peer_groups.destroy');
+Route::delete('/peer-groups/{peerGroup}', [PeerGroupController::class, 'destroy'])->name('peer_groups.destroy');
 
-Route::get('/surveys', 'SurveyController@all')->name('surveys.all');
+Route::get('/surveys', [SurveyController::class, 'all'])->name('surveys.all');
 
-Route::post('/surveys', 'SurveyController@store')->name('surveys.store');
+Route::post('/surveys', [SurveyController::class, 'store'])->name('surveys.store');
 
-Route::get('/surveys/{survey}', 'SurveyController@show')->name('surveys.show');
+Route::get('/surveys/{survey}', [SurveyController::class, 'show'])->name('surveys.show');
 
-Route::put('/surveys/{survey}', 'SurveyController@update')->name('surveys.update');
+Route::put('/surveys/{survey}', [SurveyController::class, 'update'])->name('surveys.update');
 
-Route::delete('/surveys/{survey}', 'SurveyController@destory')->name('surveys.destroy');
+Route::delete('/surveys/{survey}', [SurveyController::class, 'destroy'])->name('surveys.destroy');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
