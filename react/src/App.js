d7a9497f16bf639695440568afef94ece5c85497
Tristan Engen <tengen@my.bcit.ca>
2022-03-17 11:53:50 -0700

    survey and newsurvey components added

--- react/src/App.js
+++ react/src/App.js
@@ -9,7 +9,6 @@ import HomePage from './pages/HomePage';
 import PrivacyPage from './pages/PrivacyPage';
 import NotFoundPage from './pages/NotFoundPage';
 import ProfilePage from './pages/ProfilePage';
-import NewSurveyPage from './pages/NewSurveyPage';
 // Routing stuff
 import {
   BrowserRouter,
@@ -27,7 +26,6 @@ function App() {
         <Route path="/" element={<HomePage />} exact />
         <Route path="/privacy" element={<PrivacyPage />} exact />
         <Route path="/profilepage" element={<ProfilePage />} exact />
-        <Route path="/newsurvey" element={<NewSurveyPage />} exact />
         <Route path="/login" element={<LoginPage />} exact />
         <Route path="/signup" element={<SignupPage />} exact />
         <Route path="/createprofile" element={<CreateProfilePage />} exact />
