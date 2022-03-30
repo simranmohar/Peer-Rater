4fc08695a2ce24b1921c3e489689a86288133b4d
Harry Seo (Jong Hwan Seo) <jseo51@my.bcit.ca>
2022-03-29 21:08:48 -0700

    Made changes to Survey model to allow for categories to be initialized.

--- web/app/Models/Survey.php
+++ web/app/Models/Survey.php
@@ -5,13 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 
 class Survey extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'peer_group_id'
+        'peer_group_id',
+        'categories'
+    ];
+
+    protected $casts = [
+        'categories' => 'array'
     ];
 
     public function categories() {
