d37b5dae1a2db24776b19b204ec8ef6bd9bd4496
benjaminlui <benjaminlui00@gmail.com>
2022-04-02 17:26:13 -0700

    get group data

--- react/src/components/Group.js
+++ react/src/components/Group.js
@@ -23,31 +23,41 @@ const cardStyle = {
 }
 
 function Group(props) {
-    const [group, setNewGroup] = useState('')
-    const getNewGroup = () => {
-        axios.get('http://praterlaravel.azurewebsites.net/api/peer-groups')
-        .then(res => {
-            console.log(res.data)
-            res.data.forEach((data) => console.log(data.description));
-            console.log(res.data[0].description)
-            setNewGroup(res.data[0].description)
-            // console.log(res.data.description)
-        }).catch(err => {
-            console.log(err)
-        })
-    }
+    const [fetchedData, setFetchedData] = useState([]);
+
+    React.useEffect(() => {
+        const getData = async () => {
+            const data = await axios.get('http://praterlaravel.azurewebsites.net/api/peer-groups');
+            setFetchedData(data);
+        };
+        getData();
+    }, []);
+
+    console.log("data: ", fetchedData);
+
+    var groups = fetchedData;
+    console.log(groups.length)
+
     return (
-        <div class="row">
-            <div style={cardStyle.card_v1}>
-                <div className="card__body">
-                    <img style={cardStyle.image} src={props.img}/>
-                    {group && <h2 className="card__title">{group}</h2>}
-                    <h3 className="card__title">Milestone 1</h3>
-                    <p className="card__description">{props.description}</p>
+        <div>
+            {/* {fetchedData.data ? <h2>{fetchedData.data[0].description}</h2> : null} */}
+            <div class="row">
+                <div style={cardStyle.card_v1}>
+                    <div className="card__body">
+                        <img style={cardStyle.image} src={props.img} />
+                        {fetchedData.data && <h2 className="card__title">{fetchedData.data[0].description}</h2>}
+                        <h3 className="card__title">Milestone 1</h3>
+                        <p className="card__description">{props.description}</p>
+                    </div>
+                    <button className="card__btn">View Ratings</button>
                 </div>
-                <button className="card__btn">View Ratings</button>
-                <button className="card__btn" onClick={getNewGroup}>Get New Group</button>
             </div>
+            {/* {groups.map((group, key) => {
+                <div key={key}>
+                    {group ? <h6>{group[key].description}</h6> : null}
+                </div>
+            }
+            )} */}
         </div>
     );
 }
